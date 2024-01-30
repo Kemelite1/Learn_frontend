a2f95b28982637c523e9b2ce978d3ba387d9dfee
Kemelite1 <osikemelite42@gmail.com>
2024-01-30 11:27:15 +0100

    Added loaders to the configuration file.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path')
 module.exports = {
-    mode: 'production',
-    entry: path.resolve(__dirname, 'src/index.js'),
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js'),
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test:/\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ]
     }
 }
