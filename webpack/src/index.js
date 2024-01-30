8d3a835e6dca8625d5b84248e7532e9135e7f5b9
Kemelite1 <osikemelite42@gmail.com>
2024-01-30 13:50:02 +0100

    created an assets folder to keep images in

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,2 +1,11 @@
 import generateJoke from './generateJoke';
+import '../styles/main.scss'
+import holberton from './assets/holberton.jpg'
+
+const holbertonImg = document.getElementById('holbertonimg')
+holbertonImg.src = holberton
+
+
+
 console.log(generateJoke())
+console.log('welcome to the good life');
