d1a61f4bdc57ae3896b4ec957e0a085cda7a8966
Kemelite1 <osikemelite42@gmail.com>
2024-01-30 12:54:07 +0100

    Added source-map to help with debugging

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -8,7 +8,9 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
+        clean: true,
     },
+    devtool: 'source-map',
     devServer: {
         static: {
             directory: path.resolve(__dirname, 'dist')
