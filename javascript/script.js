b9ed66a11f1c6a4028c9ef7a232a7285fb36bc16
Kemelite1 <osikemelite42@gmail.com>
2024-04-02 10:49:22 +0100

    creating variables, reassigning and incrementing

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// let countButton = document.getElementById('counter').innerText = 2;
+
+// let firstSet = 20;
+// let secondSet = 30;
+// let count = firstSet + secondSet;
+// console.log(count);
+
+//create two variables, myAge and humanDogRatio
+//multiply the two together and store the result in myDogAge
+//log myDogAge to the console
+// let myAge = 20;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+//reassigning and incrementing
+//create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+//Decrease it down to 25, and then finally increase it to 70
+//let bonusPoints = 50;
+//bonusPoints = bonusPoints + 50;
+//bonusPoints = bonusPoints - 75;
+//bonusPoints = bonusPoints + 45;
+//console.log(bonusPoints);
