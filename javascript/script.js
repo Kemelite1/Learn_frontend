93f513d7b6b3a5b4f9f09bdfd02c863803e92a35
kemelite1 <osikemelite42@gmail.com>
2024-04-02 18:07:22 +0100

    display the welcome message

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,12 @@
 'use strict';
 
-let username = "bovi"
-let message = "You have five new notifications"
-console.log(username +"" + ", " + message + "!");
+let welcomeElement = document.getElementById('welcome-message');
+let userName = 'Trevor ';
+let greeting = 'Welcome onboard';
+let message = greeting + ' ' + userName;
 
-let messageToUser = username + ", " + message + "!"
-console.log(messageToUser);
-
-let myName = 'Trevor Job';
-let greeting = 'Hello, my name is';
-
-let myGreeting = greeting + ' ' + myName + '.';
-console.log(myGreeting);
-
-
-console.log(10 + 10); //20 - number
-console.log("10" + 10); //1010 - string
-console.log(20 + "10"); //2010 - string
-
-
-
-
+function welcomeMessage(){
+    welcomeElement.innerText = message + '👋';
+    console.log(message);
+}
+welcomeMessage();
