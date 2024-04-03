c38ddea54955afbc3c15ab9c1edefb9dab4e4653
kemelite1 <osikemelite42@gmail.com>
2024-04-03 13:46:51 +0100

    A BlackJack game - making use of array to store cards - Math.random to generate random numbers - Math.floor to remove decimals - if...else statement - for loop

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,59 @@
 'use strict';
 
-//checks if a person is old enough to enter the nightclub (21)
-let age = 25;
-//ternary operator
-age >= 21 ? console.log("You can enter the nightclub") : console.log("You are too young");
+//a blackjack game
+//a hand that gets close to/21 but never above 21. if you get 21, you are out.
+let messageElement = document.getElementById('message-paragraph');
+let sumElement = document.getElementById('sum-paragraph');
+let cardsElement = document.getElementById('cards-paragraph');
+//create two variables, firstCard and secondCard, and set their values to a random number between 2 and 11 
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [ firstCard, secondCard]
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-if (age >= 25){
-    console.log("You can enter");
-} else{
-    console.log("You are too young");
+//create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard + secondCard;
+
+function getRandomCard(){
+    let randomCard = Math.floor(Math.random() * 12) + 2;
+    if (randomCard === 1){
+        return 11;
+        } else if (randomCard > 10){
+            return 10;
+        } else {
+            return randomCard;
+        }
 }
 
-// let age = prompt("What is your age?");
-// if (age < 25){
-//     console.log("You are not old enough to enter the nightclub!");
-// } else{
-//         console.log("You can enter");
-// }
+function startGame(){
+    resetGame();
+}
+
+function resetGame(){
+    for (let i = 0; i < cards.length; i++){
+    }
+    if (sum <= 20){
+        message = `Do you want to draw a new card? 😉`;
+    } else if (sum === 21){
+        message = `You've got Blackjack! 🤑`;
+        hasBlackJack = true;
+    } else{
+        message = `You're out of the game! 😥`;
+        isAlive = false;
+    }
+    messageElement.textContent = message;
+    sumElement.textContent = `Sum: ${sum}`;
+    cardsElement.textContent = `Cards: ${cards}`;
+}
+
+function newCard(){
+    let card = getRandomCard() //card variable
+    sum += card; //add card to sum variable
+    cards.push(card); //add card to cards array
+    resetGame();
+
+}
+
+
