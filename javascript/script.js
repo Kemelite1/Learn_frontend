f7a4e2fb485d1694115ec9c31a061663658c3770
kemelite1 <osikemelite42@gmail.com>
2024-04-07 16:08:42 +0100

    - use local storage to save leads - add a button to clear leads

--- javascript/script.js
+++ javascript/script.js
@@ -5,17 +5,51 @@
 let inputElement = document.getElementById('input-field');
 let saveButton = document.getElementById('save-btn');
 let listItems = document.getElementById('list-items')
+let deleteButton = document.getElementById('delete-btn');
 let myLeads = [];
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+
+//check if there are leads in the local storage
+//if there are leads in the local storage, assign them to myLeads
+//and render them
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
+//function to render the leads
+        function renderLeads(){
+            let items = ""; //holds all the list items
+            for (let i = 0; i < myLeads.length; i++) { //loops through the myLeads array
+                items += `<li>
+                <a href='${myLeads[i]}' target="_blank"> ${myLeads[i]}</a>
+                </li>`; //adds the list items to the items variable
+            }
+            listItems.innerHTML = items; //displays the list items
+        }
+
+//add an event listener to the save button
+//when the button is clicked, save the input value to myLeads
+//then render the leads
+//finally, clear the input field
+//and save the leads to the local storage
     saveButton.addEventListener('click', () => {
         myLeads.push(inputElement.value);
-        for (let i = 0; i < myLeads.length; i++) {
-            const li = document.createElement('li');
-            li.textContent = myLeads[i];
-            listItems.appendChild(li);
-            // listItems.innerHTML += `<li>${myLeads[i]}</li>`;
-        }
+        renderLeads();
+        inputElement.value = '';
+        localStorage.setItem('myLeads', JSON.stringify(myLeads));
 
         })
 
         
+
+        //add an event listener to the delete button
+        //when the button is double clicked, clear the local storage
+        //and the myLeads array
+        //finally, render the leads
+        deleteButton.addEventListener('dblclick', () => {
+            localStorage.clear();
+            myLeads = [];
+            renderLeads();
+        })
