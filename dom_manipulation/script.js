8624ed5f9ec8d31af03540e2c53ef9ecffd71ba9
kemelite1 <osikemelite42@gmail.com>
2024-05-31 20:04:45 +0100

    Selecting elements in the DOM

---
+++ dom_manipulation/script.js
@@ -0,0 +1,10 @@
+let headingText = document.getElementById('heading');
+let listItems = document.getElementsByClassName('list-items')
+let Items = document.getElementsByTagName('li')
+let container = document.querySelector('div');
+let containers = document.querySelectorAll('div');
+
+headingText.innerHTML = 'Hello, series lovers!';
+listItems[2].innerHTML = 'Hello, series lovers!';
+
+console.log(containers);
