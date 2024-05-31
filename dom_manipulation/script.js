4dfae424c4e913ed5a5a76edfc65c2297288e528
kemelite1 <osikemelite42@gmail.com>
2024-05-31 20:55:29 +0100

    Styling elements

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -1,10 +1,13 @@
-let headingText = document.getElementById('heading');
-let listItems = document.getElementsByClassName('list-items')
-let Items = document.getElementsByTagName('li')
-let container = document.querySelector('div');
-let containers = document.querySelectorAll('div');
+let title = document.querySelector('#heading');
+let listItems = document.querySelectorAll('.list-items');
 
-headingText.innerHTML = 'Hello, series lovers!';
-listItems[2].innerHTML = 'Hello, series lovers!';
 
-console.log(containers);
+title.style.backgroundColor = 'orange';
+
+// for (let i = 0; i < listItems.length; i++) {
+//     listItems[i].style.color = 'red';
+// }
+
+listItems.forEach(item => {
+    item.style.color = 'red';
+});
