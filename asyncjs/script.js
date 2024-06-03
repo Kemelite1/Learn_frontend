deacce05009c97fcf7765d8663f32dea340355df
kemelite1 <osikemelite42@gmail.com>
2024-06-03 18:25:42 +0100

    Calback - A function that is to be executed after another function has finished executing. - In JavaScript, functions are objects. Because of this, functions can take functions as arguments,  and can be returned by other functions. Functions that do this are called higher-order functions.  Any function that is passed as an argument is called a callback function.

--- asyncjs/script.js
+++ asyncjs/script.js
@@ -1,27 +1,37 @@
-//synchronous
+// callback
+let first = (call_second) => {
+    console.log(' first step done. calling second step');
+    call_second();
+};
 
-console.log(' i');
+let second = () => {
+    console.log(' a callback');
+};
+first(second);
 
-console.log(' am');
+let stock = {
+    fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+    liquid: ['water', 'ice'],
+    holder: ['cone', 'cup', 'stick'],
+    toppings: ['chocolate', 'peanuts']
 
-console.log(' a');
-
-console.log(' synchronous');
-
-console.log(' script');
-
-console.log(' file');
-
-//asynchronous
-console.log(' i');
-
-setTimeout(() => {
-    console.log(' am now');
-}, 4000);
-
-console.log(' a');
-console.log(' asynchronous');
-console.log(' script');
-console.log(' file');
+};
 
 
+let order = (fruit_name, liquid_name, call_production) => {
+
+    setTimeout(() => {
+        console.log(`${stock.fruits[fruit_name]} and ${stock.liquid[liquid_name]} was selected`);
+
+        call_production();
+    }, 2000);
+
+};
+
+let production = () => {
+    setTimeout(() => {
+        console.log('production started');
+
+    }, 0000);
+};
+order(3, 1, production);
