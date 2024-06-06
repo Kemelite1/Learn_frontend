b4cf7d4b0003952176c5d53b9853b86b1d065681
kemelite1 <osikemelite42@gmail.com>
2024-06-06 10:19:29 +0100

    promises

--- asyncjs/script.js
+++ asyncjs/script.js
@@ -1,50 +1,35 @@
-let stock = {
-    fruits: ['strawberry', 'grapes', 'banana', 'apple'],
-    liquid: ['water', 'ice'],
-    holder: ['cone', 'cup', 'stick'],
-    toppings: ['chocolate', 'peanuts']
+const posts = [
+    {title: 'Post One', body: 'Software engineering is a great career choice'},
+    {title: 'Post Two', body: 'Nature is beautiful'},
+];
 
+let getPosts = () => {
+    setTimeout(() => {
+
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+
+        });
+        document.body.innerHTML = output;
+
+    }, 1000)
 };
 
-
-let order = (fruit_name, call_production) => {
-
-    setTimeout(() => {
-        console.log(`${stock.fruits[fruit_name]} was selected`);
-
-        call_production();
-
-    }, 2000);
-
-};
-
-let production = () => {
-    setTimeout(() => {
-        console.log('production started');
-
+let createPost = (post) => {
+    return new Promise((resolve, reject) =>{
         setTimeout(() => {
-            console.log('the fruit has been chopped');
-
-            setTimeout(() => {
-                console.log(`${stock.liquid[0]} and ${stock.liquid[1]} was added`);
-
-                setTimeout(() => {
-                    console.log('the machine was started');
-                    setTimeout(() => {
-                        console.log(`${stock.holder[1]} was selected`);
-                        setTimeout(() => {
-                            console.log(`${stock.toppings[0]} was added`);
-                            setTimeout(() => {
-                                console.log('ice cream was served');
-                            }, 2000);
-                        }, 3000);
-
-                    }, 2000);
-                },2000)
-            }, 1000);
-
-        }, 2000);
-
-    }, 0000);
+            posts.push(post);
+            const error = false;
+            if(!error){
+                resolve();
+            } else {
+                reject('Error: Something went wrong');
+            }
+        }, 2000)
+    })
 };
-order(3, production);
+
+createPost({title: 'Post Three', body: 'Web development and App development'})
+.then(getPosts)
+.catch(err => console.log(err));
