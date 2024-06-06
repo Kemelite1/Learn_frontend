84029a798833b65ed856e5ffd977440d46dd1401
kemelite1 <osikemelite42@gmail.com>
2024-06-06 19:52:13 +0100

    consuming promise

--- asyncjs/script.js
+++ asyncjs/script.js
@@ -1,57 +1,22 @@
-const posts = [
-    {title: 'Post One', body: 'Software engineering is a great career choice'},
-    {title: 'Post Two', body: 'Nature is beautiful'},
-];
-
-let getPosts = () => {
-    setTimeout(() => {
-
-        let output = '';
-        posts.forEach((post, index) => {
-            output += `<li>${post.title}</li>`;
-
-        });
-        document.body.innerHTML = output;
-
-    }, 1000)
-};
-
-let createPost = (post) => {
-    return new Promise((resolve, reject) =>{
-        setTimeout(() => {
-            posts.push(post);
-            const error = false;
-            if(!error){
-                resolve();
-            } else {
-                reject('Error: Something went wrong');
-            }
-        }, 2000)
-    })
-};
-
-// createPost({title: 'Post Three', body: 'Web development and App development'})
-// .then(getPosts)
-// .catch(err => console.log(err));
 
 
-// Async / Await
-
-async function init(){
-    await createPost({title: 'Post Three', body: 'Web development and App development'});
-    getPosts();
+const getUsersData = function (user){
+    fetch(`https://jsonplaceholder.typicode.com/users/${user}`).then(function(response){
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+    });
 }
 
-init();
+getUsersData('1');
 
-// Async / Await with fetch
+// arrow function
+const getUserData = (user) => {
 
-async function fetchUsers(){
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    fetch(`https://jsonplaceholder.typicode.com/users/${user}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
 
-    const data = await res.json();
 
-    console.log(data);
 }
-
-fetchUsers();
+getUserData('2');
