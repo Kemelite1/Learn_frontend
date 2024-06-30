bb2b1687a9f08097d4a36e3ac48318caf8003527
kemelite1 <osikemelite42@gmail.com>
2024-06-30 17:48:20 +0100

    The console

---
+++ modernjs/script.js
@@ -0,0 +1,18 @@
+console.table({Name: 'Bovi', Email: 'bovin@yahoo.com', Age: 25, Gender: 'Male',});
+
+console.group('Avengers');
+console.log('The Avengers');
+console.log('Age of Ultron');
+console.log('Civil War');
+console.log('Infinity War');
+console.log('End Game');
+console.groupEnd();
+
+const styles = `
+    padding: 10px;
+    background-color: white;
+    color: black;
+    font-weight: bold;
+`;
+
+console.log('%cHello JavaScript', styles);
