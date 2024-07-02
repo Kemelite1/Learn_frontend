d5bbb11a1f2874955808dfd2959b9a7105dd1549
kemelite1 <osikemelite42@gmail.com>
2024-07-02 22:13:34 +0100

    Comments

--- modernjs/script.js
+++ modernjs/script.js
@@ -1,18 +1,19 @@
-console.table({Name: 'Bovi', Email: 'bovin@yahoo.com', Age: 25, Gender: 'Male',});
+//console.table({Name: 'Bovi', Email: 'bovin@yahoo.com', Age: 25, Gender: 'Male',});
 
-console.group('Avengers');
+/* console.group('Avengers');
 console.log('The Avengers');
 console.log('Age of Ultron');
 console.log('Civil War');
 console.log('Infinity War');
 console.log('End Game');
 console.groupEnd();
+*/
 
-const styles = `
-    padding: 10px;
-    background-color: white;
-    color: black;
-    font-weight: bold;
-`;
+// const styles = `
+//     padding: 10px;
+//     background-color: white;
+//     color: black;
+//     font-weight: bold;
+// `;
 
-console.log('%cHello JavaScript', styles);
+// console.log('%cHello JavaScript', styles);
