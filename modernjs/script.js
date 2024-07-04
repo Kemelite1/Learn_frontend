de7e27e01050df09caec2b865ac398881717d7e0
kemelite1 <osikemelite42@gmail.com>
2024-07-04 16:05:34 +0100

    Nesting, concat and spread operator

--- modernjs/script.js
+++ modernjs/script.js
@@ -1,49 +1,63 @@
-//Array - special type of object and a data structure that can hold more than one value at a time
-//Array is a list of items
+//nesting arrays - arrays within arrays
+const avengers = ['Captain America', 'Black Widow', 'Black Panther', 'Iron Man', 'Thor', 'Hulk', 'Hawkeye'];
+const justiceLeague = ['Batman', 'Superman', 'Wonder Woman', 'Flash', 'Green Lantern', 'Cyborg', 'Aquaman'];
+//console.log(avengers, justiceLeague);
+avengers.push(justiceLeague);
+//console.log(avengers);
 
-//Array literal
-const Avengers = ['Hulk', 'Ironman', 'Captain America', 'Doctor Strange', 'Black Panther',  'Thor', 'Spiderman', 'Antman', 'Black Widow', 'Hawkeye', 'Captain Marvel'];
-console.log(Avengers);
-//length of array
-console.log(Avengers.length);
+x = avengers[7][2];
+//console.log(x);
 
-result = `My favourite Avenger is ${Avengers[2]}`;
-result = `My favourite Avengers are ${Avengers[1]}, ${Avengers[2]}, and ${Avengers[4]}`;
-console.log(result);
+const superHeroes = [avengers, justiceLeague];
+//console.log(superHeroes);
 
-//Array constructor
-const clubs = new Array('Real Madrid', 'Barcelona', 'Manchester United', 'Liverpool', 'Bayern Munich', 'Chelsea', 'Arsenal', 'Manchester City', 'Juventus', 'AC Milan');
-data = `My favourite football club is ${clubs[2]}`;
-clubs[1] = 'Paris Saint Germain';
-//clubs.length = 5;
-console.log(clubs);
-console.log(data);
+y = superHeroes[1][3];
+y = superHeroes[0][0];
+//console.log(y);
 
-//Array methods
-//push() - adds an element to the end of the array
-clubs.push('Inter Milan');
-//pop() - removes the last element from the array
-clubs.pop();
-//unshift() - adds an element to the beginning of the array
-clubs.unshift('Atletico Madrid');
-//shift() - removes the first element from the array
-clubs.shift();
-//splice() - removes or replaces an element in the array
-clubs.splice(2, 1, 'Borussia Dortmund');
-//sort() - sorts the elements in the array
-clubs.sort();
-//reverse() - reverses the order of the elements in the array
-clubs.reverse();
-//concat() - combines two or more arrays
-const newClubs = ['Napoli', 'Roma', 'Lazio'];
-const allClubs = clubs.concat(newClubs);
-console.log(allClubs);
-//join() - converts the array into a string
-const stringClubs = clubs.join(', ');
-console.log(stringClubs);
-//includes() - checks if an element is present in the array
-console.log(clubs.includes('Real Madrid'));
-//slice() - extracts a portion of the array
-const sliceClubs = clubs.slice(0, 5).reverse().toString().toUpperCase();
-console.log(sliceClubs);
-console.log(clubs);
+//concatenating arrays - combining arrays by taking all elements from one array and adding them to another array
+x = avengers.concat(justiceLeague);
+
+//spread operator - takes all elements from an array and adds them to another array
+x = [...avengers, ...justiceLeague];
+
+//flatten an array - takes an array within an array and flattens it into a single array
+x = avengers.flat();
+//console.log(x);
+
+const num = [1, 2, [3, 4, 5], 6, [7, 8, 9]];
+n = num.flat();
+//console.log(n);
+
+//static methods on array object - methods that are called on the array object itself
+//isArray() - checks if the value is an array
+x = Array.isArray(avengers);
+
+//from() - creates a new array from an array-like object
+x = Array.from('software');
+
+//of() - creates a new array from the arguments passed to it
+x = Array.of(1, 2, 3, 4, 5);
+//console.log(x);
+
+//check
+const task = [1, 2, 3, 4, 5];
+task.reverse();
+task.push(0);
+task.unshift(6)
+console.log(task);
+
+const task2 = [1, 2, 3, 4, 5];
+const task3 = [5, 6, 7, 8, 9, 10];
+//task2.pop();
+x = task2.concat(task3);
+//console.log(x);
+
+//solution 2
+const arr3 = task2.slice(0, 4).concat(task3);
+//console.log(arr3);
+
+//solution 3
+const arr4 = [...task2, ...task3];
+arr4.splice(4, 1);
+console.log(arr4);
